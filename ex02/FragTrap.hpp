78de17b220b6ec7cb9863c7efea317bb9dc522d3
Thaleia Simitopoulou <thaliasimi@gmail.com>
2024-11-10 16:14:57 +0200

    FraTrap class created and added functions, no tests yet

---
+++ ex02/FragTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef FRAGTRAP_H
+# define FRAGTRAP_H
+
+# include "ScavTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+public:
+	FragTrap();										//Default constructor
+	FragTrap(std::string name);						//Parameterized constructor
+	FragTrap(const FragTrap& other);				//Copy consturctor
+	FragTrap& operator=(const FragTrap& other);		//Copy assignment operator
+	FragTrap(FragTrap&& other) noexcept;			//Move constructor
+	FragTrap& operator=(FragTrap&& other) noexcept;	//Move assignment operator
+	~FragTrap();									//Default destructor
+
+	void	highFivesGuys(void);
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,41 @@
+#include "FragTrap.hpp"
+
+int	main(void)
+{
+	ScavTrap	scav1("ShifuSkav");
+	ScavTrap	scav2("AtlasSkav");
+	ClapTrap	clap1("ClapptonClap");
+
+	std::cout << "\033[92m";
+	std::cout << "-----PRESENTING CLAPTRAPPERS!-----\n";
+	std::cout << scav1.getName() << " is scav1" << std::endl;
+	std::cout << scav2.getName() << " is scav2" << std::endl;
+	std::cout << clap1.getName() << " is clap1" << std::endl;
+	std::cout << "\033[34m";
+
+	std::cout << "------SETTING ATTACK DAMAGE!-----\n"; //if ClapTrap
+	clap1.setAttackDamage(7);
+
+	// std::cout << "---------PRINTING STATUS!--------\n"; //if ScavTrap
+	// scav1.printStatus();
+	// scav2.printStatus();
+
+	std::cout << "\033[31m";
+	std::cout << "----------FIGHT STARTS!----------\n";
+	scav2.guardGate();
+	scav1.attack(scav2.getName());
+	scav2.takeDamage(scav1.getAttackDamage());
+	clap1.attack(scav2.getName());
+	scav2.takeDamage(clap1.getAttackDamage());
+	scav2.beRepaired(60);
+	scav2.attack(scav1.getName());
+	scav1.takeDamage(scav2.getAttackDamage());
+	scav2.attack(clap1.getName());
+	clap1.takeDamage(scav2.getAttackDamage());
+	clap1.beRepaired(5);
+	scav2.attack(clap1.getName());
+	clap1.takeDamage(scav2.getAttackDamage());
+	std::cout << "-----------FIGHT ENDS!-----------\n";
+	std::cout << "\033[0m";
+	return (0);
+}
